a3ed8d9b6cdbb940a1bae04173ef5dafe233e038
Wojciech Zysk <wojtekpo@interia.eu>
2020-03-31 17:55:04 +0200

    Update MainActivity_ViewModel

--- app/src/main/java/com/example/youtubebg/ViewModels/MainActivity_ViewModel.java
+++ app/src/main/java/com/example/youtubebg/ViewModels/MainActivity_ViewModel.java
@@ -2,12 +2,24 @@ package com.example.youtubebg.ViewModels;
 
 import com.example.youtubebg.Models.Search_Response;
 
-public class MainActivity_ViewModel {
+import
+
+import javax.sql.DataSource;
+
+import androidx.lifecycle.ViewModel;
+import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity_ViewModel extends ViewModel {
 
     private Search_Response list = new Search_Response();
 
-
-
+Observable<Search_Response> search_responseObservable = Observable
+        .fromIterable(DataSource.c)
+.subscribeOn(Schedulers.io())
+.observeOn(AndroidSchedulers.mainThread());
 
 
 }
